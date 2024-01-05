09fe518b2ea09b815650b46cc1cc63ecfad69def
furaihan <28698437+furaihan@users.noreply.github.com>
2024-01-05 22:46:46 +0700

    feat(model): add mahasiswa - Added 'Mahasiswa' model to the project - Includes properties: NIM, Nama, Alamat, Jurusan - Decorated 'NIM' property with [Key] attribute for primary key - Model resides in the 'Dirakitin.Models' namespace

---
+++ Models/Mahasiswa.cs
@@ -0,0 +1,13 @@
+﻿namespace Dirakitin.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Mahasiswa
+    {
+        [Key]
+        public string NIM {  get; set; }
+        public string Nama { get; set; }
+        public string Alamat { get; set; }
+        public string Jurusan { get; set; }
+    }
+}
