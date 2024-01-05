3aac147f412be950c378ae2d9e83add0cf5480da
furaihan <28698437+furaihan@users.noreply.github.com>
2024-01-05 22:53:36 +0700

    feat(database): add AppDbContext for Mahasiswa model
    - Added 'AppDbContext' class to handle database context
    - Inherits from 'DbContext'
    - Constructor takes 'DbContextOptions<AppDbContext>' as parameter
    - Includes DbSet property for 'Mahasiswa' model
    - Model is associated with the 'Mahasiswa' table in the database

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Dirakitin.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+        public DbSet<Mahasiswa> Mahasiswa { get; set; } = null;
+    }
+}
