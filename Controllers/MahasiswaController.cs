a0b5824dd56f5fc817eb7371f0cc213a2ffda8b6
furaihan <28698437+furaihan@users.noreply.github.com>
2024-01-06 13:38:11 +0700

    feat(controller): enhance MahasiswaController with additional actions and logging
    - Added logging statements to MahasiswaController for better request tracking
    - Implemented a 'GetMahasiswa' action to retrieve Mahasiswa by NIM
    - Updated 'Post' action to use the 'nameof' operator for CreatedAtAction method

--- Controllers/MahasiswaController.cs
+++ Controllers/MahasiswaController.cs
@@ -1,5 +1,6 @@
 ﻿using Dirakitin.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dirakitin.Controllers
 {
@@ -10,16 +11,32 @@ namespace Dirakitin.Controllers
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] Mahasiswa mahasiswa)
         {
-            logger.LogInformation("Mahasiswa Post Requested");
+            logger.LogInformation("/Mahasiswa Post Requested");
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
+            logger.LogInformation("Mahasiswa is valid");
 
             context.Mahasiswa.Add(mahasiswa);
             await context.SaveChangesAsync();
+            logger.LogInformation("Mahasiswa added successfully in database");
 
-            return CreatedAtAction("GetMahasiswa", new { id = mahasiswa.NIM }, mahasiswa);
+            return CreatedAtAction(nameof(GetMahasiswa), new {nim = mahasiswa.NIM}, mahasiswa);
         }
+        // GET: api/Mahasiswa/123456
+        [HttpGet("{nim}")]
+        public async Task<IActionResult> GetMahasiswa(string nim)
+        {
+            var mahasiswa = await context.Mahasiswa.FindAsync(nim);
+
+            if (mahasiswa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(mahasiswa);
+        }
+
     }
 }
