4de811223c6f71c9f0f86fcc1cf7d828a9b899ce
furaihan <28698437+furaihan@users.noreply.github.com>
2024-01-06 07:37:30 +0700

    feat(controller): add MahasiswaController, update Mahasiswa model with validation attributes
    - Added 'MahasiswaController' to handle Mahasiswa-related API requests
    - Injected ILogger<MahasiswaController> and 'AppDbContext' into the controller's constructor
    - Implemented a 'Post' action to handle POST requests for Mahasiswa creation
    - Updated 'Mahasiswa' model with validation attributes for NIM, Nama, and Jurusan

---
+++ Controllers/MahasiswaController.cs
@@ -0,0 +1,25 @@
+﻿using Dirakitin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dirakitin.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MahasiswaController(ILogger<MahasiswaController> logger, AppDbContext context) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Mahasiswa mahasiswa)
+        {
+            logger.LogInformation("Mahasiswa Post Requested");
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            context.Mahasiswa.Add(mahasiswa);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMahasiswa", new { id = mahasiswa.NIM }, mahasiswa);
+        }
+    }
+}
--- Models/Mahasiswa.cs
+++ Models/Mahasiswa.cs
@@ -4,10 +4,14 @@
 
     public class Mahasiswa
     {
+        [Required(ErrorMessage = "NIM is required")]
+        [StringLength(12, MinimumLength = 5, ErrorMessage = "NIM must be between 5 and 12 characters")]
         [Key]
         public string NIM {  get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Mahasiswa Name is required")]
         public string Nama { get; set; }
         public string Alamat { get; set; }
+        [Required(ErrorMessage = "Mahasiswa Jurusan is required")]
         public string Jurusan { get; set; }
     }
 }
