b733a9819b4aa5e056807ab4ade8b506adf62a61
Zhafar Al Fathi <28698437+furaihan@users.noreply.github.com>
2024-01-17 18:48:30 +0700

    feat(controller): add DosenController with POST and GET actions
    - Added 'DosenController' to handle operations related to the 'Dosen' model.
    - Includes a POST action to add a new Dosen to the database.
    - Includes a GET action to retrieve Dosen information based on NIP.

---
+++ Controllers/DosenController.cs
@@ -0,0 +1,42 @@
+﻿using Dirakitin.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dirakitin.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DosenController(ILogger<DosenController> logger, AppDbContext context) : Controller
+    {
+        // POST: /Dosen
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Dosen dosen)
+        {
+            logger.LogInformation("/Dosen Post Requested");
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            logger.LogInformation("Dosen is valid");
+
+            context.Dosen.Add(dosen);
+            await context.SaveChangesAsync();
+            logger.LogInformation("Dosen added successfully in database");
+
+            return CreatedAtAction(nameof(GetDosen), new {nip = dosen.NIP}, dosen);
+        }
+        // GET: /Dosen/123456
+        [HttpGet("{nip}")]
+        public async Task<IActionResult> GetDosen(string nip)
+        {
+            var dosen = await context.Dosen.FindAsync(nip);
+
+            if (dosen == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(dosen);
+        }
+    }
+}
