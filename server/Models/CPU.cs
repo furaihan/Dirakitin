d0ab3a76105b637427b87c9e379350fa567ba483
Zhafar Al Fathi <28698437+furaihan@users.noreply.github.com>
2024-02-02 18:39:37 +0700

    Restructure project into client and server folders
    - Move frontend code to new client folder
    - Move backend code to new server folder
    - Update import paths and package.json files to reflect new structure
    - No functional changes, just project organization

---
+++ server/Models/CPU.cs
@@ -0,0 +1,18 @@
+﻿namespace Dirakitin.Models
+{
+    public class CPU
+    {
+        public int CPUID { get; set; }
+        public string Model { get; set; }
+        public int Core { get; set; }
+        public int Thread { get; set; }
+        public int BaseClock { get; set; }
+        public int BoostClock { get; set; }
+        public int TDP { get; set; }
+        public int Price { get; set; }
+        public int L1Cache { get; set; }
+        public int L2Cache { get; set; }
+        public int L3Cache { get; set; }
+
+    }
+}
---
+++ server/Models/Socket.cs
@@ -0,0 +1,15 @@
+﻿namespace Dirakitin.Models
+{
+    public class Socket
+    {
+        public enum SocketType
+        {
+            Intel,
+            AMD
+        }
+        public int SocketID { get; set; }
+        public string SocketName { get; set; }
+        public SocketType Type { get; set; }
+        public List<CPU> CPUs { get; set; }
+    }
+}
